aac5ec61dde8f298e158f8e88b9bef903f5b80ed
Lance Smith <lance.matthew.smith@gmail.com>
2019-06-23 15:50:19 -0700

    asdf

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,8 @@ import "../css/reset";
 import "../css/style";
 import Game from "./game";
 
+window.dirname = __dirname;
+
 window.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById("game");
   const game = new Game(canvas);
