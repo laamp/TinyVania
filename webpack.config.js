f623224ecdcec9366731fb604141180fa6ee9300
Lance Smith <lance.matthew.smith@gmail.com>
2019-06-23 13:56:31 -0700

    Build adjustments

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/js/index.js',
   output: {
     filename: 'main.js',
-    publicPath: path.resolve(__dirname + "/dist/"),
+    publicPath: "/dist/",
     path: path.resolve(__dirname, "dist")
   },
   module: {
