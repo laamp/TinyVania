6e7933caea4bf3e79d22d44b7ad55e2fb262f74f
Lance Smith <lance.matthew.smith@gmail.com>
2019-06-25 10:57:37 -0700

    Remove commented code

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,6 @@ const path = require('path');
 
 module.exports = {
   entry: './src/js/index.js',
-  // output: {
-  //   filename: 'main.js',
-  //   path: path.resolve(__dirname)
-  // },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
